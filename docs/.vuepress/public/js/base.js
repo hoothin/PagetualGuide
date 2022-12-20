13cfcc9cf5b55a232b6cf3127060638f9b18a6e9
hoothin <rixixi@gmail.com>
2022-12-20 16:26:00 +0800

    Update base.js

--- docs/.vuepress/public/js/base.js
+++ docs/.vuepress/public/js/base.js
@@ -1,7 +1,10 @@
 if (location.hostname === 'pagetual.hoothin.com') {
     window.addEventListener('pushState', e => {
         setTimeout(() => {
-            window.history.replaceState(undefined, document.title, location.href.replace("/PagetualGuide", ""));
+            window.history.replaceState(undefined, document.title, location.href.replace("/PagetualGuide/", "/"));
         },1);
+        setTimeout(() => {
+            window.history.replaceState(undefined, document.title, location.href.replace("/PagetualGuide/", "/"));
+        },500);
     });
 }
