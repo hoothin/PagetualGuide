05fc67cf029840f1f3025d2d1e569659b24b084c
hoothin <rixixi@gmail.com>
2023-04-19 18:31:29 +0800

    Update base.js

--- docs/.vuepress/public/js/base.js
+++ docs/.vuepress/public/js/base.js
@@ -1,5 +1,5 @@
 if (location.hostname === 'pagetual.hoothin.com') {
-    window.addEventListener('pushState', e => {
+    window.addEventListener('pagetual_pushState', e => {
         setTimeout(() => {
             window.history.replaceState(undefined, document.title, location.href.replace("/PagetualGuide/", "/"));
         },1);
